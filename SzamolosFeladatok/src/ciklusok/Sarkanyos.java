096e816a186b16edfead2dea592c6eff1386e929
PetiDominik <petidomi05@gmail.com>
2023-03-02 14:15:28 +0100

    sarkanyos feladat elkezdve

---
+++ SzamolosFeladatok/src/ciklusok/Sarkanyos.java
@@ -0,0 +1,22 @@
+
+package ciklusok;
+
+public class Sarkanyos {
+    
+    public static void main(String[] args) {
+        
+        int i = 1;
+        int j = 1;
+        
+        while (((7*i) + (11*j)) != 145) {
+            j = 1;
+            while (((7*i) + (11*j)) < 145) {
+                j++;
+            }
+            i++;
+        }
+        System.out.printf("7*%d(%d) + 11*%d(%d) = 145\n", i, i*7, j, j*11);
+        
+    }
+    
+}
